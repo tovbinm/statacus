0a26c9c008aec5f39b92fdbd7f52975cd7c69ce9
Matthew Tovbin <tovbinm@gmail.com>
2012-05-01 21:37:29 -0700

    minor fix

--- src/main/scala/stats.scala
+++ src/main/scala/stats.scala
@@ -31,9 +31,11 @@ class Statsd(host: String, port: Int = 8125, prefix: String = "") {
   val addr = new SocketAddr(Addr.getByName(host), port)
   val chan = DatagramChannel.open
   val rand = new Random
+  val prefix_str = if (prefix!=null && prefix.length>0 && prefix.endsWith(".")) prefix 
+    			         else { prefix + "." } 
 
   def counter[T](key: String, by: Int = 1, rate: Double = 1.0)(implicit p: Pusher[T]) = new {
-   var c : Long = 0L
+	 var c : Long = 0L
      def inc = { c = c + by; Statsd.this.inc(key, by, rate) }
      def dec = { c = c - by; Statsd.this.dec(key, by, rate) }
      def total = c
@@ -46,13 +48,13 @@ class Statsd(host: String, port: Int = 8125, prefix: String = "") {
     count(if(by < 0) by else -by, rate)(key)(p)
 
   def count[T](by: Int = 1, rate: Double = 1.0)(keys: String*)(implicit p: Pusher[T]) =
-    sample(rate, (for(k <- keys) yield "%s%s:%d|c".format(prefix,k, by)): _*)(p)
+    sample(rate, (for(k <- keys) yield "%s%s:%d|c".format(prefix_str,k, by)): _*)(p)
 
   def time[T](key: String,`val`: Int, rate: Double = 1.0)(implicit p: Pusher[T]) =
-    sample(rate, "%s%s:%d|ms".format(prefix, key, `val`))(p)
+    sample(rate, "%s%s:%d|ms".format(prefix_str, key, `val`))(p)
 
   private def sample[T](rate: Double, stats: String*)(implicit pusher: Pusher[T]) =
     if(rate >= 1.0) pusher.push(addr, chan, stats: _*)
     else pusher.push(addr, chan,
-        (for(stat <- stats; d <- Some(rand.nextDouble) if (d <= rate)) yield "%s%s|@%f".format(prefix, stat, rate)): _*)
+        (for(stat <- stats; d <- Some(rand.nextDouble) if (d <= rate)) yield "%s|@%f".format(stat, rate)): _*)
 }
